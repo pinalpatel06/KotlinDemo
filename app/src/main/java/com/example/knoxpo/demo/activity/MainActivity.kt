36d4b95e9aeb24d70d69c26493500d8cb62d86b2
pinal <pinal.knoxpo@gmail.com>
2018-01-03 16:41:55 +0530

    implemented mvvm arch

--- app/src/main/java/com/example/knoxpo/demo/activity/MainActivity.kt
+++ app/src/main/java/com/example/knoxpo/demo/activity/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.knoxpo.demo.activity
 
+import android.content.Intent
+import android.os.Bundle
 import android.support.v4.app.Fragment
 import com.example.knoxpo.demo.fragment.MainFragment
+import com.example.knoxpo.demo.fragment.PostItemNavigator
+import com.example.knoxpo.demo.fragment.PostViewModel
+import com.example.knoxpo.demo.utils.obtainViewModel
 
-class MainActivity : ToolbarActivity() {
+class MainActivity : ToolbarActivity(), PostItemNavigator {
+    override fun openPostDetail(postId: Long) {
+        val intent = Intent(this, DetailActivity::class.java).apply {
+            putExtra(DetailActivity.EXTRA_TITLE, postId)
+        }
+
+        startActivity(intent)
+    }
+
+    lateinit var mViewModel: PostViewModel
 
     override fun getContentFragment(): Fragment {
-        return MainFragment()
+        val fragment = MainFragment()
+        return fragment
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mViewModel = obtainViewModel().apply {
+           // openPostDetail()
+        }
+
+    }
+
+    fun obtainViewModel(): PostViewModel= obtainViewModel(PostViewModel::class.java)
 }
